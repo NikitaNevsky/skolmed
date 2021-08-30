e890ea67c3205c6f9aed0fc1c8bab3f101f5b9ad
NikitaNevsky <40342871+NikitaNevsky@users.noreply.github.com>
2021-08-30 15:49:40 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@ $('.burger').click(function () {
     $('.menu').toggleClass('d-block');
 });
 
+$("a.scroll-to").click(function() {
+    $("html, body").animate({
+       scrollTop: $($(this).attr("href")).offset().top + "px"
+    }, {
+       duration: 500,
+       easing: "swing"
+    });
+    return false;
+});
+
 $('.doctors__slider').owlCarousel({
     items:1,
     loop:false,
